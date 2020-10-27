866818d9d933ed7e2576df24f2541ad6c25d2277
eduardotorressubias <edu.torres9@hotmail.com>
2020-10-27 14:54:52 +0100

    Add files via upload

---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //control player
+    public float horizontalMove;
+    public float verticalMove;
+    private Vector3 playerInput;
+    public CharacterController player;
+    
+    public float playerSpeed;
+    private Vector3 movePlayer;
+    public float gravity = 9.8f;
+    public float fallVelocity;
+    public float jumpForce;
+
+    //stats
+    public int curHealth;
+    public int maxHealth = 5;
+
+
+    //cameras
+    public Camera mainCamera;
+    private Vector3 camForward;
+    private Vector3 camRight;
+
+    private MenuManager menuManager;
+
+    void Start()
+    {
+        menuManager = FindObjectOfType<MenuManager>();
+        player = GetComponent<CharacterController>();
+
+        curHealth = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxis("Horizontal");
+        verticalMove = Input.GetAxis("Vertical");
+
+        playerInput = new Vector3(horizontalMove, 0, verticalMove);
+        playerInput = Vector3.ClampMagnitude(playerInput, 1);
+
+        camDirection();
+
+        movePlayer = playerInput.x * camRight + playerInput.z * camForward;
+
+        movePlayer = movePlayer * playerSpeed;
+
+        player.transform.LookAt(player.transform.position + movePlayer);
+
+        SetGravity();
+
+        playerSkills();
+
+        player.Move(movePlayer * Time.deltaTime);
+
+        if (curHealth > maxHealth)
+        {
+            curHealth = maxHealth;
+        }
+        if(curHealth <= 0)
+        {
+            Die();
+        }
+
+        
+
+    }
+    //Funcion para la direccion a la que mira la camara
+    void camDirection()
+    {
+        camForward = mainCamera.transform.forward;
+        camRight = mainCamera.transform.right;
+
+        camForward.y = 0;
+        camRight.y = 0;
+
+        camForward = camForward.normalized;
+        camRight = camRight.normalized;
+    }
+
+    //Fucion para Jump
+
+    public void playerSkills()
+    {
+        if(player.isGrounded && Input.GetButtonDown("Jump"))
+        {
+            fallVelocity = jumpForce;
+            movePlayer.y = fallVelocity;
+        }
+    }
+    //funcion para la gravedad
+    void SetGravity()
+    {
+        if (player.isGrounded)
+        {
+            fallVelocity = -gravity * Time.deltaTime;
+            movePlayer.y = fallVelocity;
+        }
+        else
+        {
+            fallVelocity -= gravity * Time.deltaTime;
+            movePlayer.y = fallVelocity;
+        }
+    }
+    public void Die()
+    {
+        menuManager.GameOver();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Win")
+            menuManager.WinScene();
+    }
+}
