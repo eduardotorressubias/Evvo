d5b409e638d41804d143283fdfde9ddee79295c5
DaniCollado <72010031+DaniCollado@users.noreply.github.com>
2020-12-09 10:03:35 +0100

    Prueba_commit_Dani

--- Assets/scripts/EnemyShoot.cs
+++ Assets/scripts/EnemyShoot.cs
@@ -7,36 +7,39 @@ public class EnemyShoot : MonoBehaviour
 {
 
     public float speed;
-    public float highRotation;
-
-    private Vector3 target;
-    private Vector3 rotation;
 
     private PlayerController evvo;
-    private Rigidbody rb;
-
-
+    private EnemyController enemy;
+    private Vector3 target;
+    private Vector3 rotation;
+    private Vector3 rotation2;
+   
+    Quaternion rot2;
+    Quaternion rot1;
+    public Quaternion currentAngle;
     void Start()
     {
         evvo = FindObjectOfType<PlayerController>();
-        rb = GetComponent<Rigidbody>();
+        enemy = FindObjectOfType<EnemyController>();
+        target = new Vector3(evvo.player.transform.position.x, evvo.player.transform.position.y+0.7f, evvo.player.transform.position.z);
 
-        target = new Vector3(evvo.player.transform.position.x, evvo.player.transform.position.y + 0.7f, evvo.player.transform.position.z);
-        rotation = new Vector3(evvo.player.transform.position.x, highRotation, evvo.player.transform.position.z);
+
+        rotation = new Vector3(evvo.player.transform.position.x, enemy.AlturaEnemigo+1.2f, evvo.player.transform.position.z);
+        //rot1 = Quaternion.LookRotation(rotation);
+        //rotation2 = new Vector3(evvo.player.transform.position.x, enemy.AlturaEnemigo -10f, evvo.player.transform.position.z);
+        //rot2 = Quaternion.LookRotation(rotation2);
 
         transform.LookAt(rotation);
+        //transform.rotation = rot1;
+
 
     }
 
     void Update()
     {
-
         transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
 
-        Vector3 lookatposition = transform.position + rb.velocity;
-
-        transform.LookAt(lookatposition);
-
+        //this.transform.rotation = Quaternion.Slerp(this.transform.rotation, rot2, 1f );
 
         if (transform.position.x == target.x && transform.position.y == target.y)
         {
@@ -57,5 +60,41 @@ public class EnemyShoot : MonoBehaviour
         Destroy(gameObject);
     }
 
-    
+    //public float speed = 2f;
+    //public float lifeDuration = 2f;
+    //private EnemyController enemy;
+    //private PlayerController player;
+
+    //private float lifeTimer;
+
+    //void Start()
+    //{
+    //    lifeTimer = lifeDuration;
+    //    enemy = FindObjectOfType<EnemyController>();
+
+    //}
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+    //    //make the bullet move
+    //    transform.position += enemy.agent.transform.position * speed * Time.deltaTime;
+    //    //transform.rotation = Quaternion.LookRotation(enemy.agent.transform.forward);
+    //    //destruir el bullet
+    //    lifeTimer -= Time.deltaTime;
+    //    if(lifeTimer <= 0f)
+    //    {
+    //        Destroy(gameObject);
+    //    }
+    //}
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    if(other.tag == "Player" || other.tag == "Scene")
+    //    {
+
+    //        Destroy(gameObject);
+    //    }
+
+
+    //}
 }
