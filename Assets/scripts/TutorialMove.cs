f99e9e645d54a28be8625c5621e28a249a22167e
eduardotorressubias <edu.torres9@hotmail.com>
2021-01-31 22:03:45 +0100

    tutorial+pause+enemyRepair
    se ha hecho que la puerta se habra al acabar los 3 triggers del tutorial, la pausa ahora desactiva todos los demas canvas, y el enemigo ahora siempre tiene la altura adecuada

--- Assets/scripts/TutorialMove.cs
+++ Assets/scripts/TutorialMove.cs
@@ -13,13 +13,24 @@ public class TutorialMove : MonoBehaviour
     static private bool tutorial1 = true;
     static private bool tutorial2 = true;
     static private bool tutorial3 = true;
+    private PlayerAnimation animator;
 
     private OptionsMenu pause;
+    
     void Start()
     {
         player = FindObjectOfType<PlayerController>();
         pause = FindObjectOfType<OptionsMenu>();
+        animator = FindObjectOfType<PlayerAnimation>();
         VideoPlayer.loopPointReached += skip;
+    }
+    private void Update()
+    {
+        //Debug.Log("1" + tutorial1 + " 2 " + tutorial2 + " 3 " + tutorial3);
+        if (tutorial1 == false && tutorial2 == false && tutorial3 == false)
+        {
+            animator.OpenDoor();
+        }
     }
 
     private void OnTriggerStay(Collider other)
@@ -27,42 +38,36 @@ public class TutorialMove : MonoBehaviour
 
         if ((player.horizontalMove != 0 && other.tag == "Player" && tutorial1 == true) || (player.verticalMove != 0 && other.tag == "Player" && tutorial1 == true))
         {
-            Debug.Log("1 "+tutorial1);
+            
             tutorial1 = false;
-           
+            //Debug.Log("1 " + tutorial1);
             skip2();
         }
 
         if (Input.GetKey("space") && other.tag=="Player"&& tutorial2==true)
         {
-            Debug.Log("2 "+tutorial2);
-            tutorial2 = false;
             
+            tutorial2 = false;
+            //Debug.Log("2 " + tutorial2);
             skip2();
         }
         
         if(Input.GetKeyDown(KeyCode.Mouse0) && other.tag == "Player" && tutorial3==true)
         {
-            Debug.Log("3" +tutorial3);
-            tutorial3 = false;
             
+            tutorial3 = false;
+            //Debug.Log("3" + tutorial3);
             skip2();
+            animator.OpenDoor();
         }
     }
     
     void skip(VideoPlayer vp){}
     void skip2()
-    {
-        //if (pause.menuIsOpen)
-        //{
-        //    tutorial.SetActive(false);
-        //}
-       
-
+    {       
         tutorial.SetActive(false);
         //player.PlayTime();
         trigger.SetActive(false);
-
 
     }
 }
