31ad72ef2d11f25b9e082df267c52bdd7252061e
eduardotorressubias <edu.torres9@hotmail.com>
2021-02-04 23:39:20 +0100

    MousePosition

--- Assets/BETA/Mainmenu/Scripts/MouseController.cs
+++ Assets/BETA/Mainmenu/Scripts/MouseController.cs
@@ -6,20 +6,24 @@ public class MouseController : MonoBehaviour
 {
     public Texture2D CursorArrow;
     public Texture2D CursorHand;
+    Vector2 position;
 
     void Start()
     {
+        position = new Vector2(15, 10);
         //Cursor.visible = false;
-        Cursor.SetCursor(CursorArrow, Vector2.zero, CursorMode.ForceSoftware);
+        Cursor.SetCursor(CursorArrow, position, CursorMode.ForceSoftware);
+        
     }
+
 
     public void OnMouseEnter()
     {
-        Cursor.SetCursor(CursorHand, Vector2.zero, CursorMode.ForceSoftware);
+        Cursor.SetCursor(CursorHand, position, CursorMode.ForceSoftware);
     }
 
     public void OnMouseExit()
     {
-        Cursor.SetCursor(CursorArrow, Vector2.zero, CursorMode.ForceSoftware);
+        Cursor.SetCursor(CursorArrow, position, CursorMode.ForceSoftware);
     }
 }
